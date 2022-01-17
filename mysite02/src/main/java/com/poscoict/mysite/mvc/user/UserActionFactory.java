51463c1a467f52befa5035575c572347121e055f
Kim-JW <bbam03@naver.com>
2022-01-17 10:50:56 +0900

    mysite02 - user login, join build

--- mysite02/src/main/java/com/poscoict/mysite/mvc/user/UserActionFactory.java
+++ mysite02/src/main/java/com/poscoict/mysite/mvc/user/UserActionFactory.java
@@ -12,6 +12,14 @@ public class UserActionFactory extends ActionFactory {
 		
 		if("joinform".equals(actionName)) {
 			action = new joinFormAction();
+		} else if ("join".equals(actionName)) {
+			action = new JoinAction();
+		} else if ("joinsuccess".equals(actionName)) {
+			action = new JoinSuccessAction();
+		} else if ("loginform".equals(actionName)) {
+			action = new LoginFormAction();
+		} else if ("login".equals(actionName)) {
+			action = new LoginAction();
 		} else {
 			action = new MainAction();
 		}
