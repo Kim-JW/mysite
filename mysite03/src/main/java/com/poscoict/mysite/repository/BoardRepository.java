7f07f2ff70c5191d1379d543417798e37a9b7b4e
Kim-JW <bbam03@naver.com>
2022-01-25 19:54:51 +0900

    mysite03 complete

--- mysite03/src/main/java/com/poscoict/mysite/repository/BoardRepository.java
+++ mysite03/src/main/java/com/poscoict/mysite/repository/BoardRepository.java
@@ -6,59 +6,34 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import com.poscoict.mysite.vo.BoardVo;
 
 @Repository
 public class BoardRepository {
+	
+	@Autowired
+	private SqlSession sqlSession ;
+	
 	public int insert(BoardVo boardVo) {
-		int result = 0;
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		
-		try {
-			conn = getConnection();
-			
-			if(boardVo.getGroupNo() == null) {
-				String sql = "insert  into board values(null, ?, ?, 0, (select ifnull(max( g_no ), 0 ) + 1 from board a), 1, 0, now(), ?)";
-				pstmt = conn.prepareStatement(sql);
-
-				pstmt.setString(1, boardVo.getTitle());
-				pstmt.setString(2, boardVo.getContents());
-				pstmt.setLong(3, boardVo.getUserNo());
-			} else {
-				String sql = "insert  into board values(null, ?, ?, 0, ?, ?, ?, now(), ?)";
-				pstmt = conn.prepareStatement(sql);
-
-				pstmt.setString(1, boardVo.getTitle());
-				pstmt.setString(2, boardVo.getContents());
-				pstmt.setInt(3, boardVo.getGroupNo());
-				pstmt.setInt(4, boardVo.getOrderNo());
-				pstmt.setInt(5, boardVo.getDepth());
-				pstmt.setLong(6, boardVo.getUserNo());
-			}
-			
-			result = pstmt.executeUpdate();
-		} catch (SQLException e) {
-			System.out.println("error:" + e);
-		} finally {
-			try {
-				if(pstmt != null) {
-					pstmt.close();
-				}
-				if(conn != null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}		
-
-		return result;
+		return sqlSession.insert("board.insert", boardVo);
 	}
 	
 	public List<BoardVo> findAllByPageAndKeword(String keyword, Integer page, Integer size) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("keyword", keyword);
+		map.put("page", page);
+		map.put("size", size);
+		
+		sqlSession.selectList("board.findAllByPageAndKeword", map);
+		
+		
 		List<BoardVo> result = new ArrayList<>();
 		Connection conn = null;
 		PreparedStatement pstmt = null;
@@ -367,49 +342,8 @@ public class BoardRepository {
 	}
 	
 	public int getTotalCount(String keyword) {
-		int result = 0;		
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-				
-		try {
-			conn = getConnection();
-			
-			if(keyword == null || "".equals(keyword)) {
-				String sql = "select count(*) from board";
-				pstmt = conn.prepareStatement(sql);
-			} else {
-				String sql = "select count(*) from board where title like ? or contents like ?";
-				pstmt = conn.prepareStatement(sql);
-				
-				pstmt.setString(1, "%" + keyword + "%");
-				pstmt.setString(2, "%" + keyword + "%");
-			}
-			
-			rs = pstmt.executeQuery();
-			
-			if(rs.next()) {
-				result = rs.getInt(1);
-			}
-		} catch (SQLException e) {
-			System.out.println("error:" + e);
-		} finally {
-			try {
-				if(rs != null) {
-					rs.close();
-				}
-				if(pstmt != null) {
-					pstmt.close();
-				}
-				if(conn != null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
+		return sqlSession.selectOne("board.getTotalCount", "%" + keyword + "%");
 		
-		return result;
 	}
 	
 	private Connection getConnection() throws SQLException {
