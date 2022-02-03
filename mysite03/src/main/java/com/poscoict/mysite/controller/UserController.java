b9b4561a64aa42a374a7abf31c34ce25b255a57f
Kim-JW <bbam03@naver.com>
2022-02-03 09:52:54 +0900

    Validation (join)

--- mysite03/src/main/java/com/poscoict/mysite/controller/UserController.java
+++ mysite03/src/main/java/com/poscoict/mysite/controller/UserController.java
@@ -1,10 +1,15 @@
 package com.poscoict.mysite.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -27,7 +32,18 @@ public class UserController {
 	}
 	
 	@RequestMapping(value="/join", method=RequestMethod.POST)
-	public String join(UserVo userVo) {
+	public String join(@Valid UserVo userVo, BindingResult result, Model model) {
+		
+		if(result.hasErrors()) {
+//			List<ObjectError> list = result.getAllErrors();
+//			for(ObjectError error : list) {
+//				System.out.println(error);
+//			}
+			
+			model.addAllAttributes(result.getModel());
+			return "user/join";
+		}
+		
 		userService.join(userVo);
 		return "redirect:/user/joinsuccess";
 	}
