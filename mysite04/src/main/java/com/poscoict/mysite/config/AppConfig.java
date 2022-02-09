3f88aa5104c1ec78b2629d6d25ffbfd6154b6482
Kim-JW <bbam03@naver.com>
2022-02-09 12:46:01 +0900

    Mysite04 config set #2

--- mysite04/src/main/java/com/poscoict/mysite/config/AppConfig.java
+++ mysite04/src/main/java/com/poscoict/mysite/config/AppConfig.java
@@ -1,9 +1,12 @@
-package com.poscoict.mysite.config.app;
+package com.poscoict.mysite.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
+
+import com.poscoict.config.app.DBConfig;
+import com.poscoict.config.app.MyBatisConfig;
 
 @Configuration
 
@@ -11,7 +14,7 @@ import org.springframework.context.annotation.Import;
 @EnableAspectJAutoProxy
 
 @ComponentScan({"com.poscoict.mysite.service","com.poscoict.mysite.repository","com.poscoict.mysite.aspect"})
-
+@Import({DBConfig.class, MyBatisConfig.class})
 public class AppConfig {
 	
 }
