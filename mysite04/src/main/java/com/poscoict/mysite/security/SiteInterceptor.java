d1f0b3307a84422df310222b8f200fc188911d59
Kim-JW <bbam03@naver.com>
2022-02-11 12:41:48 +0900

    siteInterceptor revised

--- mysite04/src/main/java/com/poscoict/mysite/security/SiteInterceptor.java
+++ mysite04/src/main/java/com/poscoict/mysite/security/SiteInterceptor.java
@@ -1,17 +1,12 @@
 package com.poscoict.mysite.security;
 
-import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
 //import org.apache.catalina.core.ApplicationContext;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import com.poscoict.mysite.service.SiteService;
-import com.poscoict.mysite.service.UserService;
 import com.poscoict.mysite.vo.SiteVo;
 
 public class SiteInterceptor extends HandlerInterceptorAdapter {
